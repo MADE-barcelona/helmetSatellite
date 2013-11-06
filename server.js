f55fc99f9f7ae9044b76a66b47c4a7b118b04343
geekfreak <geekfreak@gmail.com>
2013-11-06 01:48:57 +0000

    refactored color logging

--- server.js
+++ server.js
@@ -29,56 +29,57 @@ var URL        = require("url") ;
 var helmetPort = 8080 ;
 
 // CONSTANTS
+var LOG_ENABLED  = true ;
 var LOG_VERBOSE  = true ;
 var SERVE_STATIC = false ;
 
 var log = function() {
-    
-  var clrFgReset   = "\x1b[39m" ; // reset
-    
-  var clrBlack     = "\x1b[30m" ; // Black
-  var clrGrey      = "\x1b[90m" ; // Grey
-  var clrLightGrey = "\x1b[37m" ; // Light Grey
-  var clrRed       = "\x1b[31m" ; // Red
-  var clrGreen     = "\x1b[32m" ; // Green
-  var clrBlue      = "\x1b[34m" ; // Blue
-  var clrYellow    = "\x1b[33m" ; // Yellow
-  var clrViolet    = "\x1b[35m" ; // Violet
-  var clrCyan      = "\x1b[36m" ; // Cyan
-
-  var message = Array.prototype.slice.call(arguments).join(clrCyan+' , '+clrFgReset);
   
-  console.log(clrBlue + new Date().toString().split(" ")[4] + clrViolet + " [" + process.pid + '] ' + clrFgReset + message + clrFgReset) ;
+  var ink = {
+    reset     : "\x1b[39m"
+  , black     : "\x1b[30m"
+  , red       : "\x1b[31m"
+  , green     : "\x1b[32m"
+  , yellow    : "\x1b[33m"
+  , blue      : "\x1b[34m"
+  , violet    : "\x1b[35m"
+  , cyan      : "\x1b[36m"
+  , lightGrey : "\x1b[37m"
+  , grey      : "\x1b[90m"     
+  }
+
+  var message = Array.prototype.slice.call(arguments).join(ink.cyan+' , '+ink.reset);
+  
+  LOG_ENABLED && console.log(ink.blue + new Date().toString().split(" ")[4] + ink.violet + " [" + process.pid + '] ' + ink.reset + message + ink.reset) ;
     
 } ;
 
 // launch bridging server
 connect.createServer(
-    connect.favicon()
-    , connect.logger({ format: "\x1b[31mhelmet:\x1b[39m :remote-addr :method :status :url :response-time" })
-    , function(request, response) { 
-    
-        function angleValid(angle) {
-          return !isNaN(parseFloat(angle)) && isFinite(angle) && (angle >= 0) && (angle <= 180) ;
-        } ; 
-
-        var angle = URL.parse(request.url).pathname.replace(/\D/,'') ; // needs parse
-
-        if (angleValid(angle)) { 
-          log(angle, CURRENT_ANGLE) ;
-          CURRENT_ANGLE = angle ; //  set current value
-        } else {
-          LOG_VERBOSE && log(request.socket.remoteAddress, request.url, angle, CURRENT_ANGLE) ;    
-        }
-                
-        response.writeHead(200, {
-            'Content-type': 'text/plain'
-        });
-        response.write(CURRENT_ANGLE+"\n") ;
-        response.end() ;
+  connect.favicon()
+  , connect.logger({ immediate: true, format: "\x1b[31mhelmet:\x1b[39m :remote-addr :method :status :url :response-time" })
+  , function(request, response) { 
+  
+      function angleValid(angle) {
+        return !isNaN(parseFloat(angle)) && isFinite(angle) && (angle >= 0) && (angle <= 180) ;
+      } ; 
         
+      var angle = URL.parse(request.url).pathname.replace(/\D/,'') ; // needs parse
+        
+      if (angleValid(angle)) { 
+        log(angle, CURRENT_ANGLE) ;
+        CURRENT_ANGLE = angle ; //  set current value
+      } else {
+        LOG_VERBOSE && log(request.socket.remoteAddress, request.url, angle, CURRENT_ANGLE) ;    
+      }
+        
+      response.writeHead(200, {
+        'Content-type': 'text/plain'
+      });
+      response.write(CURRENT_ANGLE+"\n") ;
+      response.end() ;  
+      
     }
-    
 ).listen(helmetPort) ;
 
 log("listening for helmet on " + helmetPort ) ;
